ebb413957ab4380b3f45e51cfbc9d53c27b58679
Akshay <2210019+AkshayRaman@users.noreply.github.com>
2017-11-17 10:25:27 -0800

    Change default build

--- setup.sh
+++ setup.sh
@@ -7,8 +7,12 @@
 #CXXFLAGS="-Wall -g -O0 --std=c++11" time ./waf configure --enable-tests --enable-examples --enable-modules=internet,point-to-point,applications
 
 #Using the .ns3rc file
-#CXXFLAGS="-Wall -g -O0" time ./waf configure --enable-tests --enable-examples
-CXXFLAGS="-Wall -g -O0 --std=c++11" time ./waf configure --enable-tests --enable-examples
+
+#For seasnet builds, use:
+CXXFLAGS="-Wall -g -O0" time ./waf configure --enable-tests --enable-examples
+
+#For Ubuntu builds, use:
+#CXXFLAGS="-Wall -g -O0 --std=c++11" time ./waf configure --enable-tests --enable-examples
 
 ./waf clean
 
