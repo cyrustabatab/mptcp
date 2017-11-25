443e79113d7f4a06e3ad967d2de71975d688f450
ctabatab <ctabatab@gmail.com>
2017-11-24 17:41:25 -0800

    mptcp-csma.cc is a csma network where an mptcp flow occurs between two hosts

---
+++ scratch/mptcp-csma.cc
@@ -0,0 +1,92 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Morteza Kheirkhah <m.kheirkhah@sussex.ac.uk>
+ */
+
+// Network topology
+//
+//       n0 ----------- n1
+// - Flow from n0 to n1 using MpTcpBulkSendApplication.
+
+#include <string>
+#include <fstream>
+#include "ns3/core-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/network-module.h"
+#include "ns3/packet-sink.h"
+#include "ns3/csma-module.h"
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE("MpTcpBulkSendExample");
+
+int
+main(int argc, char *argv[])
+{
+  LogComponentEnable("MpTcpSocketBase", LOG_INFO);
+
+  Config::SetDefault("ns3::Ipv4GlobalRouting::FlowEcmpRouting", BooleanValue(true));
+  Config::SetDefault("ns3::TcpSocket::SegmentSize", UintegerValue(1400));
+  Config::SetDefault("ns3::TcpSocket::DelAckCount", UintegerValue(0));
+  Config::SetDefault("ns3::DropTailQueue::Mode", StringValue("QUEUE_MODE_PACKETS"));
+  Config::SetDefault("ns3::DropTailQueue::MaxPackets", UintegerValue(100));
+  Config::SetDefault("ns3::TcpL4Protocol::SocketType", TypeIdValue(MpTcpSocketBase::GetTypeId()));
+  Config::SetDefault("ns3::MpTcpSocketBase::MaxSubflows", UintegerValue(8)); // Sink
+  //Below, we can set the congestion control algorithm
+  //Config::SetDefault("ns3::MpTcpSocketBase::CongestionControl", StringValue("RTT_Compensator"));
+  //Below, we set the path management algorithm used. N diff ports uses one IP address and different port numbers while the one we should use later when are topology is compatible, is full mesh where multiple IPs are used
+  Config::SetDefault("ns3::MpTcpSocketBase::PathManagement", StringValue("NdiffPorts"));
+
+  NodeContainer nodes;
+  nodes.Create(18);
+  
+
+  
+
+  CsmaHelper csma;
+  csma.SetChannelAttribute("DataRate", StringValue("100Mbps"));
+  csma.SetChannelAttribute("Delay", TimeValue(NanoSeconds(6560)));
+
+  NetDeviceContainer csmaDevices;
+  csmaDevices = csma.Install(nodes);
+  
+  InternetStackHelper internet;
+  internet.Install(nodes);
+
+  Ipv4AddressHelper ipv4;
+  ipv4.SetBase("10.1.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer i = ipv4.Assign(csmaDevices);
+
+  uint16_t port = 9;
+  MpTcpPacketSinkHelper sink("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), port));
+  ApplicationContainer sinkApps = sink.Install(nodes.Get(17));
+  sinkApps.Start(Seconds(0.0));
+  sinkApps.Stop(Seconds(10.0));
+
+  MpTcpBulkSendHelper source("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address(i.GetAddress(17)), port));
+  source.SetAttribute("MaxBytes", UintegerValue(0));
+  ApplicationContainer sourceApps = source.Install(nodes.Get(0));
+  sourceApps.Start(Seconds(0.0));
+  sourceApps.Stop(Seconds(10.0));
+
+  NS_LOG_INFO ("Run Simulation.");
+  Simulator::Stop(Seconds(20.0));
+  Simulator::Run();
+  Simulator::Destroy();
+  NS_LOG_INFO ("Done.");
+
+}
