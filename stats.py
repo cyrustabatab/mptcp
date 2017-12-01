7ed6c7cbec0a5c0008f64383cb30bfe46b6cbbf7
ctabatab <ctabatab@gmail.com>
2017-12-01 12:41:55 -0800

    stats.py process rtt data as well

--- stats.py
+++ stats.py
@@ -1,12 +1,16 @@
 import glob
 import numpy as np
 
-file_list = glob.glob("subflow_*.csv")
-
+file_list = glob.glob("SF_*.csv")
+rtt_list = glob.glob("RTT_*.csv")
 stats_file = open("stats.txt", "w")
-for f in file_list:
+for f,r in zip(file_list,rtt_list):
         stats_file.write(f + "\n")
 	array  = np.genfromtxt(f)
-        stats_file.write("Average: " + str(np.mean(array))+ "\n");
-        stats_file.write("Standard Deviation: " + str(np.std(array)) + "\n\n")
+        rtt_array = np.genfromtxt(r)
+        stats_file.write("CWND Average: " + str(np.mean(array))+ "\n");
+        stats_file.write("CWND Standard Deviation: " + str(np.std(array)) + "\n")
+        stats_file.write("RTT Average: " + str(np.mean(rtt_array)) + "\n");
+        stats_file.write("RTT Standard Deviation: " + str(np.std(rtt_array)) + "\n\n");
+        
 stats_file.close()
