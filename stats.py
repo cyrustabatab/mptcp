2aac46d4e7cbc27ead71ca0b25f831341fe3efef
ctabatab <ctabatab@gmail.com>
2017-12-01 12:26:27 -0800

    stats.py collects statistics about subflows

---
+++ stats.py
@@ -0,0 +1,11 @@
+import glob
+import numpy as np
+
+file_list = glob.glob("subflow_*.csv")
+
+stats_file = open("stats.txt", "w")
+for f in file_list:
+        stats_file.write(f + "\n")
+	array  = np.genfromtxt(f)
+        stats_file.write("Average: " + str(np.mean(array))+ "\n");
+        stats_file.write("Standard Deviation: " + str(np.std(array)) + "\n\n")
