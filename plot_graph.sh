c8be6aa740bebbeeadc306235316c6b6837aacd2
Akshay <2210019+AkshayRaman@users.noreply.github.com>
2017-12-05 12:42:03 -0800

    Update plot generate script

--- plot_graph.sh
+++ plot_graph.sh
@@ -10,7 +10,18 @@ echo "Start"
 
 gnuplot $1
 
-for i in *.eps; do convert $i -resize "1428x1000" -quality 100 $i.png; done
+now=$(date -u +'%Y%m%dT%H%M%S')
+
+for i in *.eps; 
+    do 
+        timestamped_file=$i"_"$now.png;
+        new_file=$i.png;
+
+        echo "Generate: " $timestamped_file
+        convert $i -resize "1428x1000" -quality 100 $timestamped_file;
+        echo "Update: " $new_file
+        /bin/cp $timestamped_file $new_file;
+    done
 
 rm -f *.eps
 
