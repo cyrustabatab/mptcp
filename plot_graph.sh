e768013670f88dbe3b38c362f47f2e34d5bf88d6
Akshay <2210019+AkshayRaman@users.noreply.github.com>
2017-12-05 14:20:26 -0800

    Add prefix to graph file

--- plot_graph.sh
+++ plot_graph.sh
@@ -11,16 +11,17 @@ echo "Start"
 gnuplot $1
 
 now=$(date -u +'%Y%m%dT%H%M%S')
+read -e -p "File prefix:" prefix
 
 for i in *.eps; 
     do 
-        timestamped_file=$i"_"$now.png;
+        old_file=$i"_"$prefix"_"$now.png;
         new_file=$i.png;
 
-        echo "Generate: " $timestamped_file
-        convert $i -resize "1428x1000" -quality 100 $timestamped_file;
+        echo "Generate: " $old_file
+        convert $i -resize "1428x1000" -quality 100 $old_file;
         echo "Update: " $new_file
-        /bin/cp $timestamped_file $new_file;
+        /bin/cp $old_file $new_file;
     done
 
 rm -f *.eps
