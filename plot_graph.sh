83e591205f38515a325c2212bef5531bce3f1d66
Akshay <2210019+AkshayRaman@users.noreply.github.com>
2017-12-05 17:47:27 -0800

    Do not generate backup

--- plot_graph.sh
+++ plot_graph.sh
@@ -11,17 +11,14 @@ echo "Start"
 gnuplot $1
 
 now=$(date -u +'%Y%m%dT%H%M%S')
-read -e -p "File prefix:" prefix
+read -e -p "File prefix: " prefix
 
 for i in *.eps; 
     do 
         old_file=$i"_"$prefix"_"$now.png;
-        new_file=$i.png;
 
         echo "Generate: " $old_file
         convert -density 300 $i -resize "1428x1000" -quality 100 -flatten $old_file;
-        echo "Update: " $new_file
-        /bin/cp $old_file $new_file;
     done
 
 rm -f *.eps
